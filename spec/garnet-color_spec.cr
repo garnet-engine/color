3b66196fdfb2673fce8fb82c2abb0f5a74ad84e1
Stephen Belanger <admin@stephenbelanger.com>
2020-09-10 19:45:52 -0700

    Add github actions

--- spec/garnet-color_spec.cr
+++ spec/garnet-color_spec.cr
@@ -6,26 +6,26 @@ include Garnet
 describe Color do
   it "should have white global" do
     white = Color::WHITE
-    white.r.should eq(1_f32)
-    white.g.should eq(1_f32)
-    white.b.should eq(1_f32)
-    white.a.should eq(1_f32)
+    white.red.should eq(1_f32)
+    white.green.should eq(1_f32)
+    white.blue.should eq(1_f32)
+    white.alpha.should eq(1_f32)
   end
 
   it "should have black global" do
     black = Color::BLACK
-    black.r.should eq(0_f32)
-    black.g.should eq(0_f32)
-    black.b.should eq(0_f32)
-    black.a.should eq(1_f32)
+    black.red.should eq(0_f32)
+    black.green.should eq(0_f32)
+    black.blue.should eq(0_f32)
+    black.alpha.should eq(1_f32)
   end
 
   it "should construct" do
     grey = Color.new(0.5_f32, 0.5_f32, 0.5_f32, 1_f32)
-    grey.r.should eq(0.5_f32)
-    grey.g.should eq(0.5_f32)
-    grey.b.should eq(0.5_f32)
-    grey.a.should eq(1_f32)
+    grey.red.should eq(0.5_f32)
+    grey.green.should eq(0.5_f32)
+    grey.blue.should eq(0.5_f32)
+    grey.alpha.should eq(1_f32)
   end
 
   it "should lerp" do
--- src/garnet-color.cr
+++ src/garnet-color.cr
@@ -1,13 +1,13 @@
 struct Garnet::Color
-  property :r
-  property :g
-  property :b
-  property :a
+  getter :red
+  getter :green
+  getter :blue
+  getter :alpha
 
   WHITE = self.new(1.0_f32, 1.0_f32, 1.0_f32, 1.0_f32)
   BLACK = self.new(0.0_f32, 0.0_f32, 0.0_f32, 1.0_f32)
 
-  def initialize(@r : Float32, @g : Float32, @b : Float32, @a : Float32)
+  def initialize(@red : Float32, @green : Float32, @blue : Float32, @alpha : Float32)
   end
 
   def lerp(other : self, t : Float32)
@@ -15,10 +15,10 @@ struct Garnet::Color
     tn = 1.0_f32 - t
 
     self.class.new(
-      tn * r + t * other.r,
-      tn * g + t * other.g,
-      tn * b + t * other.b,
-      tn * a + t * other.a,
+      tn * red + t * other.red,
+      tn * green + t * other.green,
+      tn * blue + t * other.blue,
+      tn * alpha + t * other.alpha,
     )
   end
 end
